800761d4120dc598237a353e2e6036391ff2ab41
notna <anton@drogin.ru>
2019-10-13 01:32:03 +0300

    #master - fix env

--- environments/dev/web/index.php
+++ environments/dev/web/index.php
@@ -5,7 +5,7 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-if (!file_exists(__DIR__ . '/config/db.php')) {
+if (!file_exists(__DIR__ . '/../config/db.php')) {
     die('Copy the file from config/db.example to config/db.php');
 }
 

a3eddf5ce7ae85f77986760b3a34de9485831897
notna <anton@drogin.ru>
2019-10-14 03:15:33 +0300

    #master - fix env

--- environments/dev/config/test-local.php
+++ environments/dev/config/test-local.php
@@ -5,6 +5,5 @@ return yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/web.php'),
     require(__DIR__ . '/web-local.php'),
     require(__DIR__ . '/test.php'),
-    require(__DIR__ . '/test-local.php'),
     require(__DIR__ . '/test_db.php')
 );
